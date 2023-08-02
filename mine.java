3d50950e2cdd8f7c2ccbe19420ae2a8e5d0109ba
khushi <116934492+khush1106@users.noreply.github.com>
2023-08-02 12:21:54 +0530

    Add files via upload

---
+++ mine.java
@@ -0,0 +1,569 @@
+import java.util.*;
+import java.io.*;
+class help
+{
+    void game()
+    {
+        System.out.print("###########");
+        System.out.print("    ");
+        System.out.print("###########");
+        System.out.print("    ");
+        System.out.print("#         #");
+        System.out.print("    ");
+        System.out.println("###########");
+
+        System.out.print("#          ");
+        System.out.print("    ");
+        System.out.print("#         #");
+       System.out.print("    ");
+        System.out.print("# #     # #");
+       System.out.print("    ");
+         System.out.println("#          ");
+
+        System.out.print("#          ");
+        System.out.print("    ");
+        System.out.print("#         #");
+    System.out.print("    ");
+        System.out.print("#  #   #  #");
+        System.out.print("    ");
+        System.out.println("#          ");
+
+        System.out.print("#          ");
+        System.out.print("    ");
+        System.out.print("#         #");
+       System.out.print("    ");
+        System.out.print("#   # #   #");
+        System.out.print("    ");
+        System.out.println("#          ");
+
+
+        System.out.print("#     #####");
+        System.out.print("    ");
+        System.out.print("###########");
+        System.out.print("    ");
+        System.out.print("#    #    #");
+        System.out.print("    ");
+        System.out.println("######     ");
+
+        System.out.print("#         #");
+       System.out.print("    ");
+        System.out.print("#         #");
+        System.out.print("    ");
+        System.out.print("#    #    #");
+        System.out.print("    ");
+        System.out.println("#          ");
+
+        System.out.print("#         #");
+          System.out.print("    ");
+        System.out.print("#         #");
+       System.out.print("    ");
+        System.out.print("#    #    #");
+         System.out.print("    ");
+        System.out.println("#          ");
+
+        System.out.print("#         #");
+        System.out.print("    ");
+        System.out.print("#         #");
+        System.out.print("    ");
+        System.out.print("#    #    #");
+        System.out.print("    ");
+        System.out.println("#          ");
+      
+
+        System.out.print("###########");
+        System.out.print("    ");
+        System.out.print("#         #");
+        System.out.print("    ");
+        System.out.print("#    #    #");
+        System.out.print("    ");
+        System.out.println("###########");
+    }
+    void start()
+    {
+System.out.print("###########");
+        System.out.print("    ");
+        System.out.print("###########");
+        System.out.print("    ");
+        System.out.print("###########");
+        System.out.print("    ");
+         System.out.print("###########");
+        System.out.print("    ");
+        System.out.println("###########");
+//2
+        System.out.print("#          ");
+        System.out.print("    ");
+        System.out.print("     #     ");
+        System.out.print("    ");
+        System.out.print("#         #");
+       System.out.print("    ");
+         System.out.print("#         #");
+       System.out.print("    ");
+        System.out.println("     #     ");
+//3
+         System.out.print("#          ");
+        System.out.print("    ");
+        System.out.print("     #     ");
+        System.out.print("    ");
+        System.out.print("#         #");
+    System.out.print("    ");
+        System.out.print("#         #");
+        System.out.print("    ");
+         System.out.println("     #     ");
+//4
+         System.out.print("#          ");
+        System.out.print("    ");
+        System.out.print("     #     ");
+        System.out.print("    ");
+        System.out.print("#         #");
+       System.out.print("    ");
+         System.out.print("#         #");
+        System.out.print("    ");
+         System.out.println("     #     ");
+      
+
+//5
+        System.out.print("###########");
+        System.out.print("    ");
+        System.out.print("     #     ");
+        System.out.print("    ");
+        System.out.print("###########");
+        System.out.print("    ");
+        System.out.print("###########");
+        System.out.print("    ");
+        System.out.println("     #     ");
+//6
+        System.out.print("          #");
+       System.out.print("    ");
+       System.out.print("     #     ");
+        System.out.print("    ");
+        System.out.print("#         #");
+        System.out.print("    ");
+        System.out.print("#      #   ");
+        System.out.print("    ");
+        System.out.println("     #     ");
+//7
+        System.out.print("          #");
+          System.out.print("    ");
+          System.out.print("     #     ");
+        System.out.print("    ");
+        System.out.print("#         #");
+       System.out.print("    ");
+        System.out.print("#       #  ");
+         System.out.print("    ");
+        System.out.println("     #     ");
+//8
+        System.out.print("          #");
+        System.out.print("    ");
+        System.out.print("     #     ");
+        System.out.print("    ");
+        System.out.print("#         #");
+        System.out.print("    ");
+        System.out.print("#        # ");
+        System.out.print("    ");
+        System.out.println("     #     ");
+      
+//9
+        System.out.print("###########");
+        System.out.print("    ");
+        System.out.print("     #     ");
+        System.out.print("    ");
+        System.out.print("#         #");
+        System.out.print("    ");
+        System.out.print("#         # ");
+        System.out.print("    ");
+         System.out.println("     #    ");
+    }
+    void over()
+    {
+      System.out.print("###########");
+        System.out.print("    ");
+        System.out.print("#         #");
+        System.out.print("    ");
+        System.out.print("###########");
+        System.out.print("    ");
+         System.out.println("###########");
+//2
+System.out.print("#         #");
+        System.out.print("    ");
+        System.out.print("#         #");
+        System.out.print("    ");
+        System.out.print("#          ");
+       System.out.print("    ");
+         System.out.println("#         #");
+//3
+System.out.print("#         #");
+        System.out.print("    ");
+        System.out.print("#         #");
+        System.out.print("    ");
+        System.out.print("#          ");
+    System.out.print("    ");
+        System.out.println("#         #");
+//4
+System.out.print("#         #");
+        System.out.print("    ");
+        System.out.print("#         #");
+        System.out.print("    ");
+        System.out.print("#          ");
+       System.out.print("    ");
+         System.out.println("#         #");
+      
+
+//5
+System.out.print("#         #");
+        System.out.print("    ");
+        System.out.print(" #       # ");
+        System.out.print("    ");
+        System.out.print("###########");
+        System.out.print("    ");
+        System.out.println("###########");
+//6
+System.out.print("#         #");
+       System.out.print("    ");
+       System.out.print("  #     #  ");
+        System.out.print("    ");
+        System.out.print("#          ");
+        System.out.print("    ");
+        System.out.println("#      #   ");
+//7
+System.out.print("#         #");
+          System.out.print("    ");
+          System.out.print("   #   #   ");
+        System.out.print("    ");
+        System.out.print("#          ");
+       System.out.print("    ");
+        System.out.println("#       #  ");
+//8
+        System.out.print("#         #");
+        System.out.print("    ");
+        System.out.print("    * *    ");
+        System.out.print("    ");
+        System.out.print("#          ");
+        System.out.print("    ");
+        System.out.println("#        # ");
+      
+//9
+        System.out.print("###########");
+        System.out.print("    ");
+        System.out.print("     #     ");
+        System.out.print("    ");
+        System.out.print("###########");
+        System.out.print("    ");
+        System.out.println("#         # ");
+    }
+    void win()
+    {
+System.out.print("#     #");
+    System.out.print("    ");
+    System.out.print("#");
+    System.out.print("    ");
+    System.out.println("#     #");
+
+//2
+     System.out.print("#     #");
+    System.out.print("    ");
+    System.out.print("#");
+    System.out.print("    ");
+    System.out.println("##    #");
+//3
+     System.out.print("#     #");
+    System.out.print("    ");
+    System.out.print("#");
+    System.out.print("    ");
+    System.out.println("# #   #");
+
+    //4 
+    System.out.print("#  #  #");
+    System.out.print("    ");
+    System.out.print("#");
+    System.out.print("    ");
+    System.out.println("#  #  #");
+
+    //5
+     System.out.print("# # # #");
+    System.out.print("    ");
+    System.out.print("#");
+    System.out.print("    ");
+    System.out.println("#   # #");
+
+    //6
+     System.out.print("##   ##");
+    System.out.print("    ");
+    System.out.print("#");
+    System.out.print("    ");
+    System.out.println("#    ##");
+
+    //7
+     System.out.print("#     #");
+    System.out.print("    ");
+    System.out.print("#");
+    System.out.print("    ");
+    System.out.println("#     #");
+    }
+    void lost()
+    {
+     System.out.print("#      ");
+    System.out.print("    ");
+    System.out.print("#######");
+    System.out.print("    ");
+    System.out.print("#######");
+    System.out.print("    ");
+    System.out.println("#######");
+//2
+    System.out.print("#      ");
+    System.out.print("    ");
+    System.out.print("#     #");
+    System.out.print("    ");
+    System.out.print("#      ");
+     System.out.print("    ");
+    System.out.println("   #   ");
+
+    //3
+    System.out.print("#      ");
+    System.out.print("    ");
+    System.out.print("#     #");
+    System.out.print("    ");
+    System.out.print("#      ");
+     System.out.print("    ");
+    System.out.println("   #   ");
+
+    //4
+    System.out.print("#      ");
+    System.out.print("    ");
+    System.out.print("#     #");
+    System.out.print("    ");
+    System.out.print("#######");
+     System.out.print("    ");
+    System.out.println("   #   ");
+
+    //5
+    System.out.print("#      ");
+    System.out.print("    ");
+    System.out.print("#     #");
+    System.out.print("    ");
+    System.out.print("      #");
+     System.out.print("    ");
+    System.out.println("   #   ");
+
+    //6
+    System.out.print("#      ");
+    System.out.print("    ");
+    System.out.print("#     #");
+    System.out.print("    ");
+System.out.print("      #");
+     System.out.print("    ");
+    System.out.println("   #   ");
+
+    //7
+    System.out.print("#######");
+    System.out.print("    ");
+    System.out.print("#######");
+    System.out.print("    ");
+    System.out.print("#######");
+        System.out.print("    ");
+    System.out.println("   #   ");
+    }
+}
+public class mine {
+    public static void main(String ar[])throws Exception
+    {
+        DataInputStream x=new DataInputStream(System.in);
+        Scanner sc=new Scanner(System.in);
+        help ph=new help();
+        ph.game();
+        Thread.sleep(500);
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        ph.start();
+        Thread.sleep(1000);
+        System.out.println("enter grid size");
+        int n=sc.nextInt();
+        int a[][]=new int[n][n];
+        System.out.println("enter the number of bombs");
+        int b=sc.nextInt();
+        int c=0,v=0,r,l,i,j,m=0,y,ch,z,point=0,t,k;
+        k=n;
+        k=k-1;
+        while(c!=b)
+        {
+            Random ran=new Random();
+            r=ran.nextInt(k);
+            l=ran.nextInt(k);
+           // System.out.println(r+" "+l);
+            a[r][l]=100;
+            if(r==0)
+            {
+                if(l==0)
+                {
+                a[r][l+1]=a[r][l+1]+1;
+                 a[r+1][l+1]=a[r+1][l+1]+1;
+                 a[r+1][l]=a[r+1][l]+1;
+                }
+                else if(l==(n-1))
+                {
+                a[r][l-1]=a[r][l-1]+1;
+            a[r+1][l-1]=a[r+1][l-1]+1;
+                    a[r+1][l]=a[r+1][l]+1;
+                }
+                else
+                {
+             a[r][l+1]=a[r][l+1]+1;
+                 a[r+1][l+1]=a[r+1][l+1]+1;
+                  a[r][l-1]=a[r][l-1]+1;
+            a[r+1][l-1]=a[r+1][l-1]+1;
+                 a[r+1][l]=a[r+1][l]+1;
+                }
+            }
+            else if(r==(n-1))
+                {
+                   if(l==0)
+                   {
+                    a[r][l+1]=a[r][l+1]+1;
+                    a[r-1][l+1]=a[r-1][l+1]+1;
+                    a[r-1][l]=a[r-1][l]+1;
+                   }
+                   else if(l==(n-1))
+                   {
+                     a[r][l-1]=a[r][l-1]+1;
+                     a[r-1][l-1]=a[r-1][l-1]+1;
+                     a[r-1][l]=a[r-1][l]+1;
+                   }
+                   else
+                   {
+                    a[r][l+1]=a[r][l+1]+1;
+                    a[r-1][l+1]=a[r-1][l+1]+1;
+                    a[r][l-1]=a[r][l-1]+1;
+                     a[r-1][l-1]=a[r-1][l-1]+1;
+                    a[r-1][l]=a[r-1][l]+1;
+                   }
+
+                }
+                else
+                {
+                    if(l==0)
+                    {
+                        a[r][l+1]=a[r][l+1]+1;
+                        a[r-1][l+1]=a[r-1][l+1]+1;
+                        a[r+1][l+1]=a[r+1][l+1]+1;
+                        a[r+1][l]=a[r+1][l]+1;
+                        a[r-1][l]=a[r-1][l]+1;
+
+                    }
+                    else if(l==(n-1))
+                    {
+                    a[r][l-1]=a[r][l-1]+1;
+                    a[r-1][l-1]=a[r-1][l-1]+1;
+                    a[r+1][l-1]=a[r+1][l-1]+1;
+                    a[r+1][l]=a[r+1][l]+1;
+                    a[r-1][l]=a[r-1][l]+1;
+                    }
+                    else
+                    {
+                    a[r][l+1]=a[r][l+1]+1;
+                    a[r][l-1]=a[r][l-1]+1;
+                    a[r-1][l]=a[r-1][l]+1;
+                    a[r-1][l+1]=a[r-1][l+1]+1;
+                    a[r-1][l-1]=a[r-1][l-1]+1;
+                    a[r+1][l-1]=a[r+1][l-1]+1;
+                    a[r+1][l]=a[r+1][l]+1;
+                    a[r+1][l+1]=a[r+1][l+1]+1;
+                    }
+                }
+                c++;
+                }
+//making new array
+String na[][]=new String[n][n];
+   for(i=0;i<n;i++)
+   {
+    for(j=0;j<n;j++)
+    na[i][j]="|__|";
+   }
+    for(i=0;i<n;i++)
+            {
+                for(j=0;j<n;j++)
+                System.out.printf("%7s",na[i][j]);
+                System.out.println();
+            }
+   ab:while(m!=n*n)
+   {
+    System.out.println("enter row");
+        y=sc.nextInt();
+        y=y-1;
+        System.out.println("enter colun");
+        z=sc.nextInt();
+        z=z-1;
+        System.out.println("enter choice 1-play 2-flag 3-doubt 4-exit");
+        ch=Integer.parseInt(x.readLine());
+        switch (ch)
+        {
+            case 1: 
+            if(a[y][z]>=100)
+            {
+                ph.game();
+                Thread.sleep(200);
+                System.out.println();
+                System.out.println();
+                ph.over();
+                Thread.sleep(1000);
+                break ab;
+            }
+            else
+            {
+                t=a[y][z];
+                na[y][z]=" "+t+" ";
+                 for(i=0;i<n;i++)
+            {
+                for(j=0;j<n;j++)
+                System.out.printf("%7s",na[i][j]);
+                System.out.println();
+            }
+            break;
+            }
+            case 2:
+            na[y][z]="|> ";
+            for(i=0;i<n;i++)
+            {
+                for(j=0;j<n;j++)
+                System.out.printf("%7s",na[i][j]);
+                System.out.println();
+            }
+            if(a[y][z]>=100)
+            point++;
+            break;
+            case 3:
+            na[y][z]="? ";
+            for(i=0;i<n;i++)
+            {
+             for(j=0;j<n;j++)
+                System.out.printf("%7s",na[i][j]);
+                System.out.println();   
+            }
+            break;
+            case 4:
+           ph.game();
+           Thread.sleep(200);
+           System.out.println();
+           System.out.println();
+           ph.over();
+           Thread.sleep(1000);
+            break ab;
+        }
+         m=m+1; 
+   }
+   System.out.println();
+   System.out.println();
+   System.out.println();
+   if(point>=b)
+   {
+    Thread.sleep(500);
+    ph.win();
+   }
+   else
+   {
+    Thread.sleep(500);
+    ph.lost();
+   }
+   System.out.println("points"+point);
+}
+    
+}
